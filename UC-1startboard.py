c98336d63c658dae9b4b4ec277e022b3984bd54a
Samrudhi <samrudhib7@gmail.com>
2021-08-04 00:12:01 +0530

    started tic tac toe game

---
+++ UC-1startboard.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# UC-1:As a Player would like
+# to start fresh with
+# resetting the board
+
+# In[ ]:
+
+
+print(" Play Tic tac Toe ")
+
+#TicTacToe 3x3 board
+TTT = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']   
+
